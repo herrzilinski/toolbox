e953dd8de7b4d50ae6b8d714a74863cc490786f6
herrzilinski <herrzilinski@gmail.com>
2021-11-04 23:42:17 -0400

    GPAC cmap update

--- handy.py
+++ handy.py
@@ -96,7 +96,7 @@ def ACF_Plot(series, lag=None, ax=None, plt_kwargs={}, removeNA=False):
     return ax
 
 
-def GPAC_cal(ry_2, Lj, Lk, cmap='GnBu_r'):
+def GPAC_cal(ry_2, Lj, Lk, cmap='RdBu'):
     if not np.array_equal(ry_2, ry_2[::-1]):
         ry_2 = np.concatenate((np.reshape(ry_2[::-1], len(ry_2)), ry_2[1:]))
     center = int((len(ry_2)-1)/2)
@@ -134,7 +134,7 @@ def GPAC_cal(ry_2, Lj, Lk, cmap='GnBu_r'):
     table = pd.DataFrame(table)
     table.columns = [str(x) for x in range(1, Lk + 1)]
 
-    sns.heatmap(table, annot=True, cmap=cmap)
+    sns.heatmap(table, annot=True, vmin=-1, vmax=1, cmap=cmap)
     plt.title(f'Generalized Partial AutoCorrelation Table')
     plt.tight_layout()
     plt.show()
