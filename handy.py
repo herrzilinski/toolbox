a3c7414ff7dbd49dc08a51235ba94939a712644d
herrzilinski <herrzilinski@gmail.com>
2021-12-10 06:06:27 -0500

    forecast bugfix

--- handy.py
+++ handy.py
@@ -906,15 +906,18 @@ class SARIMA_Estimate:
         para_r = [p for p in self.pole[1:]]
         res = np.zeros([steps + len(para_l), ])
         res[:len(para_l)] = self.series[-len(para_l):]
+        et_h = self.series[-len(para_l):] - self.y_hat[-len(para_l) - 1: -1]
 
         for i in range(steps):
             init = res[i: len(para_l) + i]
-            et_1 = differencing(init)
-            if len(et_1) < len(para_r):
-                et_1 = np.r_[np.zeros(len(para_r) - len(et_1)), et_1]
-
-            ls = init[::-1] @ para_l
-            rs = et_1[::-1] @ para_r
+            if i < len(para_l):
+                ls = init[::-1] @ para_l
+                rs = et_h[::-1] @ para_r
+                et_h[i] = 0
+                para_r[i] = 0
+            else:
+                ls = init[::-1] @ para_l
+                rs = et_h[::-1] @ para_r
             res[len(para_l) + i] = ls + rs
 
         return res[-steps:]
